117d428c353609b4142786e828cac5afcce68d54
Dapo Adeleke <adelekeoladapo@gmail.com>
2023-02-21 12:08:14 +0200

    Create pull zone application

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://api.bunny.net',
+  accessKey: 'ee7f8f66-87dc-45b2-8450-e2f794996cad3573fae8-2689-400a-bc89-4b7dee9f5407'
 };
